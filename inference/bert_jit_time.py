00d631d733882c13dea07e24ec758db25af488dc
Ningxin Zheng <Ningxin.Zheng@microsoft.com>
2021-09-14 11:55:30 +0000

    bert jit time

---
+++ inference/bert_jit_time.py
@@ -0,0 +1,60 @@
+import argparse
+import glob
+import json
+import logging
+
+import os
+import random
+import math
+
+import numpy as np
+import torch
+# from torch.functional import norm
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm, trange
+from nni.compression.pytorch.utils import get_module_by_name
+from emmental import MaskedBertConfig, MaskedBertForSequenceClassification
+from transformers import (
+    WEIGHTS_NAME,
+    AdamW,
+    BertConfig,
+    BertForSequenceClassification,
+    BertTokenizer,
+    get_linear_schedule_with_warmup,
+)
+from transformers import glue_compute_metrics as compute_metrics
+from transformers import glue_convert_examples_to_features as convert_examples_to_features
+from transformers import glue_output_modes as output_modes
+from transformers import glue_processors as processors
+from emmental.modules.masked_nn import MaskedLinear
+
+from nni.compression.pytorch import ModelSpeedup, apply_compression_results
+from nni.algorithms.compression.pytorch.pruning import L1FilterPruner, LevelPruner
+from nni.algorithms.compression.pytorch.pruning.weight_masker import WeightMasker
+from nni.algorithms.compression.pytorch.pruning.dependency_aware_pruner import DependencyAwarePruner
+ 
+def measure_time_pytorch(model, dummy_input, runtimes=200):
+    times = []
+    with torch.no_grad():
+        for runtime in range(runtimes):
+            torch.cuda.synchronize()
+            start = time.time()
+            out=model(*dummy_input)
+            torch.cuda.synchronize()
+            end = time.time()
+            times.append(end-start)
+    _drop = int(runtimes * 0.1)
+    mean = np.mean(times[_drop:-1*_drop])
+    std = np.std(times[_drop:-1*_drop])
+    return mean*1000, std*1000
+
+config = torch.load('Coarse_bert_config')
+dummy_input = torch.load('dummy_input.pth')
+
+data = (dummy_input['input_ids'], dummy_input['attention_mask'], dummy_input['token_type_ids'])
+norm_model = BertForSequenceClassification(config=config)
+jit_model = torch.jit.trace(norm_model, data)
+print(measure_time(jit_model, data))
